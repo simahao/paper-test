e1385600a94c049163ae5fead8bb139c7304fb36
simahao <simahao@sohu.com>
2021-04-12 23:16:14 +0800

    code

--- reada.py
+++ reada.py
@@ -1,19 +1,24 @@
+import numpy as np
 import pandas as pd
 
 
-def read_a(K=40000):
+def read_a(K=400):
     sk = {}
     df = pd.DataFrame(pd.read_csv("./data/neighbour150-200 copy.csv", sep=","))
     site = 0
     neighbours = {}
-    for index, row in df.iterrows():
-        benefit = int(0)
+    for (index, row) in df.iterrows():
+        benefit_sum = 0 
+        if (pd.isna(row["nei_site"])):
+            benefit_esi = row["counts"]
+            continue
         if (row["site"] != site and site != 0):
             # if there isn't more es(i)'s neighbour
             for (key, value) in neighbours.items():
-                if (benefit < K):
+                if (benefit_esi + benefit_sum < K):
                     sk[key] = value
-                    benefit = benefit + value
+                    benefit_sum = benefit_sum + value
+            benefit_esi = 0
             neighbours.clear()
         else:
             neighbours[row["nei_site"]] = row["counts"]
